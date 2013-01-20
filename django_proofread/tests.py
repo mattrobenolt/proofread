bb06920ff648389cbad2e8a5507773d2b85362e8
Matt Robenolt <matt@ydekproductions.com>
2013-01-19 23:53:29 -0800

    Print status code text into output

--- django_proofread/tests.py
+++ django_proofread/tests.py
@@ -2,9 +2,8 @@ import uuid
 import unittest
 from django.conf import settings
 from django.test.client import Client
+from django.core.handlers.wsgi import STATUS_CODE_TEXT
 
-OK = 200
-NOT_FOUND = 404
 
 ENDPOINTS = []
 for endpoint in getattr(settings, 'PROOFREAD_ENDPOINTS', []):
@@ -12,7 +11,7 @@ for endpoint in getattr(settings, 'PROOFREAD_ENDPOINTS', []):
         endpoint = endpoint + ('', 200, 'GET', None)[len(endpoint):]
     ENDPOINTS.append(endpoint)
 
-for key, status in (('SUCCESS', OK), ('FAILURES', NOT_FOUND)):
+for key, status in (('SUCCESS', 200), ('FAILURES', 404)):
     for endpoint in getattr(settings, 'PROOFREAD_%s' % key, ()):
         ENDPOINTS.append((endpoint, status, 'GET', None))
 
@@ -33,13 +32,16 @@ class BuildTestCase(type):
         for path, status, method, data in ENDPOINTS:
             if not path.startswith('/'):
                 path = '/' + path
-            test_name = 'test_proofread_%s' % uuid.uuid4().hex[:6]
+            status_text = STATUS_CODE_TEXT.get(status, 'UNKNOWN')
+
             test = make_test(path, status, method, data)
             test.__name__ = name
             if data:
-                test.__doc__ = '%s %s %r => %d' % (method, path, data, status)
+                test.__doc__ = '%s %s %r => %d %s' % (method, path, data, status, status_text)
             else:
-                test.__doc__ = '%s %s => %d' % (method, path, status)
+                test.__doc__ = '%s %s => %d %s' % (method, path, status, status_text)
+
+            test_name = 'test_proofread_%s' % uuid.uuid4().hex[:6]
             attrs[test_name] = test
         return super(BuildTestCase, cls).__new__(cls, name, bases, attrs)
 
